b6378feadc699c7663f256ad111df2a18c8fec0d
bill-jia <bill.jiazy@gmail.com>
2015-08-23 12:44:45 -0400

    Add specs

--- e2e/main.spec.js
+++ e2e/main.spec.js
@@ -4,18 +4,59 @@ describe('The main view', function () {
   var page;
 
   beforeEach(function () {
-    browser.get('/index.html');
+    browser.get('/');
     page = require('./main.po');
   });
 
-  it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+  it('has the correct title', function() {
+    expect(browser.getTitle()).toEqual("uTunes");
   });
 
-  it('should list more than 5 awesome things', function () {
-    expect(page.thumbnailEls.count()).toBeGreaterThan(5);
+  it('should include navbar with correct data', function() {
+    expect(page.sections.count()).toEqual(4);
+    expect(page.home.getAttribute('ui-sref')).toBe(".home");
+    expect(page.news.getAttribute('ui-sref')).toBe(".news");
+    expect(page.faq.getAttribute('ui-sref')).toBe(".faq");
+    expect(page.about.getAttribute('ui-sref')).toBe(".about");
   });
 
+  it('should include sidebar with correct data', function() {
+    expect(page.mediaLinks.count()).toEqual(5);
+    expect(page.albums.getAttribute("ui-sref")).toBe(".albums.index");
+    expect(page.artists.getAttribute("ui-sref")).toBe(".artists.index");
+    // TODO: Implement playlists
+    // expect(page.playlists.getAttribute("ui-sref")).toBe(".playlists.index");
+    expect(page.producers.getAttribute("ui-sref")).toBe(".producers.index");
+    expect(page.tracks.getAttribute("ui-sref")).toBe(".tracks.index");
+  });
+
+  it ('should have a homepage', function() {
+    page.home.click();
+    var content = element(by.css("md-content h2"));
+    expect(content.getText()).toEqual("This is the home page.");
+  });
+
+  // it ('should have a newspage', function() {
+  //   page.news.click();
+  //   var content = element(by.css("md-content h2"));
+  //   expect(content.getText()).toContain("This is the news page");
+  // });
+
+  it ('should have an FAQ page', function() {
+    page.faq.click();
+    var content = element(by.css("md-content h2"));
+    expect(content.getText()).toEqual("This is the FAQ page.");
+  });
+
+  it ('should have an About page', function() {
+    page.about.click();
+    var content = element(by.css("md-content h2"));
+    expect(content.getText()).toEqual("This is the About page.");
+  });
+
+  // it ('should have an Albums page', function() {
+  //   page.albums.click();
+  //   var content = element(by.css("h2"));
+  //   expect(content.isPresent()).toBe(true);
+  // });
 });
